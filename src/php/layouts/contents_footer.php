5db8d3d42bcd1b517b4ebdae8cbc08945980a898
KyawMyoThant <kyawmyothant.connetis@gmail.com>
2026-07-15 11:11:03 +0630

    footer & pagination

---
+++ src/php/layouts/contents_footer.php
@@ -0,0 +1,135 @@
+<footer>
+	<div class="footerTop">
+		<div class="ctInner">
+			<!-- ftBannerBlock -->
+			<div class="ftBannerBlock">
+				<div class="ftImgGroup">
+					<img src="assets/img/common/ft_logo.png" alt="footer" width="356" height="274">
+					<img src="assets/img/common/ft_illustration_pc.png" alt="footer illustration pc" class="pcOnly" width="878" height="201">
+					<img src="assets/img/common/ft_illustration_sp.png" alt="footer illustration sp" class="spOnly" width="320" height="2816">
+				</div>
+				<div class="ftListGroup">
+					<dl class="ftSitemap">
+						<dt>
+							<p>体験して深める</p>
+						</dt>
+						<dd>
+							<ul>
+								<li>
+									<a href="#">出張授業</a>
+								</li>
+								<li>
+									<a href="#">森永製菓のキャラメル教室</a>
+								</li>
+								<li>
+									<a href="#">森永1 チョコ for 1 スマイル 未来ラーニング</a>
+								</li>
+								<li>
+									<a href="#">森永製菓のキャリア授業</a>
+								</li>
+								<li>
+									<a href="#">森永製菓の菓子育<br>「メルとマールのピクニック」</a>
+								</li>
+								<li>
+									<a href="#">実施校一覧（PDF）</a>
+								</li>
+								<li>
+									<a href="#">活動レポート</a>
+								</li>
+							</ul>
+							<ul>
+								<li>
+									<a href="#">MORIUM＆鶴見工場見学</a>
+								</li>
+								<li>
+									<a href="#">キッザニア東京</a>
+								</li>
+								<li>
+									<a href="#">プロギング</a>
+								</li>
+								<li>
+									<a href="#">モリナガスマイルパーク</a>
+								</li>
+							</ul>
+						</dd>
+					</dl>
+					<dl class="ftSitemap">
+						<dt>
+							<p>知識を広げる</p>
+						</dt>
+						<dd>
+							<ul>
+								<li>
+									<a href="#">食のしごと</a>
+								</li>
+								<li>
+									<a href="#">お菓子の記憶</a>
+								</li>
+								<li>
+									<a href="#">医師と考える食育</a>
+								</li>
+								<li>
+									<a href="#">食品表示</a>
+								</li>
+								<li>
+									<a href="#">森永製菓の菓子育</a>
+								</li>
+								<li>
+									<a href="#">アレルギー“だから” がないラボ</a>
+								</li>
+								<li>
+									<a href="#">森永おくち研究所</a>
+								</li>
+								<li>
+									<a href="#">バーチャル工場見学憶</a>
+								</li>
+							</ul>
+						</dd>
+					</dl>
+					<dl class="ftSitemap">
+						<dt>
+							<p>関連リンク</p>
+						</dt>
+						<dd>
+							<ul>
+								<li>
+									<a href="#">MORIUM＆鶴見工場見学</a>
+								</li>
+								<li>
+									<a href="#">モリナガスマイルパーク</a>
+								</li>
+								<li>
+									<a href="#">1 チョコ for 1 スマイル</a>
+								</li>
+								<li>
+									<a href="#">キッザニア東京「お菓子工場」</a>
+								</li>
+								<li>
+									<a href="#">アレルギー“だから” がないラボ</a>
+								</li>
+								<li>
+									<a href="#">森永おくち研究所</a>
+								</li>
+								<li>
+									<a href="#">バーチャル工場見学憶</a>
+								</li>
+								<li>
+									<a href="#">対象別（年齢別）<br class="pcOnly">コンテンツ紹介一覧</a>
+								</li>
+							</ul>
+						</dd>
+					</dl>
+				</div>
+			</div>
+			<!-- /ftBannerBlock -->
+		</div>
+
+		<!-- ftBottomBlock -->
+		<div class="ftBottomBlock">
+			<img src="assets/img/common/footer_bottom_pc.png" alt="footer" class="pcOnly" width="2560" height="814">
+			<img src="assets/img/common/footer_bottom_sp.png" alt="footer" class="spOnly" width="780" height="2202">
+
+		</div>
+		<!-- /ftBottomBlock -->
+	</div>
+</footer>
