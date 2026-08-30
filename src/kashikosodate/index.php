34396dbf0a877905f8846ec93907abf865d898ba
Myat Noe <myatnoe.connetis@gmail.com>
2026-08-30 20:55:22 +0630

    edit shusei

--- src/kashikosodate/index.php
+++ src/kashikosodate/index.php
@@ -42,90 +42,7 @@
 					<!-- /breadcrumbs -->
 
 					<!-- navList -->
-					<div class="navList">
-						<div class="navListInner">
-							<ul>
-								<li class="navItem">
-									<a href="#"><span>体験で深める</span></a>
-									<div class="popupMenu">
-										<div class="titleBlock">
-											<div class="popupTitle">体験で深める<span>Experience</span></div>
-											<button type="button" class="closeBtn">閉じる</button>
-										</div>
-										<div class="linkList">
-											<ul>
-												<li><a href="#">出張授業</a></li>
-												<li><a href="#">森永製菓のキャラメル教室</a></li>
-												<li><a href="#">森永1 チョコ for 1 スマイル<br>未来ラーニング</a></li>
-												<li><a href="#">森永製菓のキャリア授業</a></li>
-											</ul>
-											<ul>
-												<li><a href="#">森永製菓の菓子育<br>「メルとマールのピクニック」</a></li>
-												<li><a href="#">実施校一覧（PDF）</a></li>
-												<li><a href="#">活動レポート</a></li>
-												<li><a href="#">MORIUM＆鶴見工場見学</a></li>
-											</ul>
-											<ul>
-												<li><a href="#">キッザニア東京</a></li>
-												<li><a href="#">プロギング</a></li>
-												<li><a href="#">モリナガスマイルパーク</a></li>
-											</ul>
-										</div>
-									</div>
-								</li>
-								<li class="navItem">
-									<a href="#"><span>知識を広げる</span></a>
-									<div class="popupMenu">
-										<div class="titleBlock">
-											<div class="popupTitle">知識を広げる<span>Knowledge</span></div>
-											<button type="button" class="closeBtn">閉じる</button>
-										</div>
-										<div class="linkList">
-											<ul>
-												<li><a href="#">食のしごと</a></li>
-												<li><a href="#">お菓子の記憶</a></li>
-												<li><a href="#">医師と考える食育</a></li>
-											</ul>
-											<ul>
-												<li><a href="#">食品表示</a></li>
-												<li><a href="#">森永製菓の菓子育</a></li>
-												<li><a href="#">アレルギー“だから”<br>がないラボ</a></li>
-											</ul>
-											<ul>
-												<li><a href="#">森永おくち研究所</a></li>
-												<li><a href="#">バーチャル工場見学憶</a></li>
-											</ul>
-										</div>
-									</div>
-								</li>
-								<li class="navItem">
-									<a href="#"><span>関連リンク</span></a>
-									<div class="popupMenu">
-										<div class="titleBlock">
-											<div class="popupTitle">関連リンク<span>Related Links</span></div>
-											<button type="button" class="closeBtn">閉じる</button>
-										</div>
-										<div class="linkList">
-											<ul>
-												<li><a href="#">MORIUM＆鶴見工場見学</a></li>
-												<li><a href="#">モリナガスマイルパーク</a></li>
-												<li><a href="#">森永製菓の菓子育</a></li>
-											</ul>
-											<ul>
-												<li><a href="#">1 チョコ for 1 スマイル</a></li>
-												<li><a href="#">キッザニア東京「お菓子工場」</a></li>
-												<li><a href="#">アレルギー“だから”<br>がないラボ</a></li>
-											</ul>
-											<ul>
-												<li><a href="#">森永おくち研究所</a></li>
-												<li><a href="#">バーチャル工場見学憶</a></li>
-											</ul>
-										</div>
-									</div>
-								</li>
-							</ul>
-						</div>
-					</div>
+					<?php require_once("../php/layouts/contents_nav.php"); ?>
 					<!-- /navList -->
 
 					<div class="ctPage">
@@ -141,14 +58,14 @@
 
 							<div class="noticeBlock">
 								<div class="floatArea">
-									<img src="assets/img/top/info_float_img1.png" class="floatImg floatImg1" alt="" width="215" height="286" >
-									<img src="assets/img/top/experience_float_img5.png" class="floatImg floatImg2" alt="" width="375" height="408">
-									<img src="assets/img/top/experience_float_img8.png" class="floatImg floatImg3" alt="" width="356" height="310">
-									<img src="assets/img/top/info_float_img2.png" class="floatImg floatImg4" alt="" width="247" height="249">
+									<img src="../assets/img/top/info_float_img1.png" class="floatImg floatImg1" alt="" width="215" height="286" >
+									<img src="../assets/img/top/experience_float_img5.png" class="floatImg floatImg2" alt="" width="375" height="408">
+									<img src="../assets/img/top/experience_float_img8.png" class="floatImg floatImg3" alt="" width="356" height="310">
+									<img src="../assets/img/top/info_float_img2.png" class="floatImg floatImg4" alt="" width="247" height="249">
 								</div>
 								<div class="title">菓子育とは</div>
-								<p class="text">菓子育は、子どもたちのすこやかな成長を応援する<br>森永製菓の食育の取り組みです。</p>
-								<p class="text">お菓子の価値は、栄養補給だけでなく、人と人、心と心をつなぐ<br>コミュニケーションの役割であると考えています。</p>
+								<p class="text">菓子育は、<br class="spOnly">子どもたちのすこやかな成長を応援する<br>森永製菓の食育の取り組みです。</p>
+								<p class="text">お菓子の価値は、栄養補給だけでなく、<br class="spOnly">人と人、心と心をつなぐ<br>コミュニケーションの役割である<br class="spOnly">と考えています。</p>
 							</div>
 
 							<div class="twoCol">
@@ -159,10 +76,8 @@
 								<div class="textCol">
 									<div class="title">菓子育絵本<br>「メルとマールのピクニック」</div>
 									<p class="text">そこで、創造性を育みながら、菓子の役割や菓子との上手な付き合い方をたのしく学ぶことができるオリジナル菓子育絵本「メルとマールのピクニック」（協力：（株）NHKエデュケーショナル）を2018年に制作しました。<br>保育・幼児教育の現場で行われている「食への関心を高めるための工夫」をちりばめた内容で構成し、さまざまな保育シーンでご活用いただいています。</p>
-									<div class="secTextGp">
-										<div class="title subTt">あらすじ</div>
-										<p class="text">仲良しのうさぎ、メルとマールは、ある日、ビスケットを持って森へお散歩に出かけます。お菓子を持っていたことで、その森の住人たちとの出会いが、思いがけない心のふれあいを生むことになります。</p>
-									</div>
+									<div class="subTt">あらすじ</div>
+									<p class="text">仲良しのうさぎ、メルとマールは、ある日、ビスケットを持って森へお散歩に出かけます。お菓子を持っていたことで、その森の住人たちとの出会いが、思いがけない心のふれあいを生むことになります。</p>
 								</div>
 							</div>
 
