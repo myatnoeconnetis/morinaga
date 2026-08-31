af9628fbd58210f1987498e7183e775902bec4f1
MytMnSthU <myatminsithu0072@gmail.com>
2026-08-31 16:39:47 +0630

    add column1 detail page

---
+++ src/column1/detail/index.php
@@ -0,0 +1,140 @@
+<?php
+require_once("../../php/init.php");
+require_once("../../php/layouts/page_header.php");
+?>
+<title>医師と考える食育</title>
+<meta property="og:title" content="医師と考える食育">
+</head>
+
+<body>
+
+	<!-- wrapper -->
+	<div id="wrapper">
+
+		<!-- innerWrapper -->
+		<div id="innerWrapper" class="ctColumnDetail">
+
+			<!-- header -->
+			<?php require_once("../../php/layouts/contents_header.php"); ?>
+			<!-- /header -->
+
+			<!-- ctArea -->
+			<div id="ctArea">
+				<main>
+
+					<!-- breadcrumbs -->
+					<nav class="breadcrumbs">
+						<ul>
+							<li>
+								<a href="/">ホーム</a>
+							</li>
+							<li>
+								<a href="#">知る・楽しむ</a>
+							</li>
+							<li>
+								<a href="#">食育・工場見学</a>
+							</li>
+							<li>
+								<a href="#">森永製菓の食育</a>
+							</li>
+						</ul>
+					</nav>
+					<!-- /breadcrumbs -->
+
+					<!-- navList -->
+					<?php require_once("../../php/layouts/contents_nav.php"); ?>
+					<!-- /navList -->
+
+					<div class="ctPage">
+						<div class="pageBnr">
+							<h1 class="pageBnrTxt">医師と考える食育</h1>
+						</div>
+
+						<div class="ctInner">
+							<div class="borderBlock">
+								<div class="topPart">
+									<div class="textCol">
+										<div class="title">「時間栄養学」を活用して健康とダイエットを叶えよう</div>
+										<p>健康のために、一日でとりたい栄養やカロリーの目安があることは知られています。近年注目されているのが、食事に時間の概念を取り入れた「時間栄養学」です。いつ、何を、どのくらい食べるのがよいのか、柴田重信先生に伺いました。</p>
+									</div>
+									<div class="imgCol">
+										<figure>
+											<img src="../../assets/img/column1/column_img1.png" alt="" width="480" height="480">
+										</figure>
+									</div>
+								</div>
+								<div class="bottomPart">
+									<div class="imgCol">
+										<figure>
+											<img src="../../assets/img/column1/column_img2.png" alt="" width="440" height="440">
+										</figure>
+									</div>
+									<div class="textCol">
+										<div class="title">柴田 重信 先生 <span class="textSmall">（広島大学 大学院医系科学研究科 特任教授）</span>
+										</div>
+										<p>薬学博士。早稲田大学人間科学部教授などを経て2023年早稲田大学名誉教授。日本時間栄養学会理事（理事長、顧問歴任）。<br>時間栄養学や時間運動学と生活習慣病との関係、時間栄養食（クロノミール）の開発、シフトワークや時差ボケと体内時計の関係やその軽減方法の開発などの研究を行う。</p>
+									</div>
+								</div>
+							</div>
+							<div class="twoCol">
+								<div class="imgCol">
+									<figure>
+										<img src="../../assets/img/column1/column_img3.jpg" alt="" width="960" height="972">
+										<figcaption>朝食は炭水化物とたんぱく質をしっかりとって、夕食は食べ過ぎに気をつける</figcaption>
+									</figure>
+								</div>
+								<div class="textCol">
+									<div class="title">朝はしっかり、夜は控えめに</div>
+									<p>時間栄養学は、「何をどれだけ食べるか」という従来の栄養学に加えて、「いつ食べるか」という視点をプラスした新しい学問です。 <br>人間には体内時計（時計遺伝子）がありますが、そのリズムと食事（栄養）の相互作用によって、からだの働き方が変わります。たとえば、「夜遅くに食事をすると太りやすい」というようなことです。 <br>内臓には体内時計によって「活発に働く時間」「休憩する時間」が決まっていて、一日の活動を始める朝は、食事でとった栄養をエネルギーに変え、からだを休める夜間は使いきれなかったエネルギーを脂肪としてためようとします。 <br>夕食でたくさん食べ過ぎてしまうと、消費できないエネルギーが脂肪として蓄積されてしまい、当然ダイエット効果は得られません。夜間は内臓の消化や吸収能力が落ちるため、食べ過ぎや夜遅い時間の食事は内臓にも負担がかかってしまいます。<br>エネルギー消費が高い時間帯の朝食をしっかり食べ、エネルギーを多く必要としない時間帯の夕食は控えめにすること、それが時間栄養学で提唱している食事の基本です。</p>
+								</div>
+							</div>
+							<div class="twoCol">
+								<div class="textCol">
+									<div class="title">体内時計のズレを食事でリセット</div>
+									<p>体内時計がリズムよく働いていると、代謝も活性化し、運動効率もアップ。やせやすいからだにも近づきます。 <br>地球の自転は24時間周期ですが、人間の体内時計は24時間よりも少し長めなので、もともと夜型になりやすい傾向があります。夜型化が進むと、睡眠不足を招き、肥満やうつ病などのリスクが高まるといわれています。<br>体内時計のズレをリセットできるのは朝だけ。朝の光を浴びて、朝食をとることから一日をスタートさせましょう。</p>
+
+									<div class="circleTitle">●朝食は起床して時間をあけずに食べる</div>
+									<p>起きてから「すぐ」が理想で、遅くとも起床後2時間以内に食べましょう。朝食をとると血糖が上昇するため、血糖を下げるホルモン（インスリン）が出ますが、このインスリンが体内時計をリセットしてくれます。さらに、炭水化物とたんぱく質を同時に摂取することで、体内時計の時刻合わせに働く遺伝子「ピリオド」も活性化します。<br>朝・昼・夕の食事のボリュームの割合は、朝しっかり夜控えめが理想ですが、現実的なところでは朝３：昼3：夕方4くらいのバランスをめざしましょう。</p>
+
+									<div class="circleTitle">●昼食は「朝食から５時間後」に</div>
+									<p>朝食から５時間後が理想。朝８時に朝食をとったら、昼食は13時ということになります。「BMAL１（ビーマル１）」という脂肪を蓄える時計遺伝子の働きが昼に活動が鈍くなるため、脂質をとるならランチがおすすめです。</p>
+
+									<div class="circleTitle">●夕食は朝食から12時間以内にすませる</div>
+									<p>朝食から10時間以内に食べるのが理想です。遅くても12時間以内をめざしましょう。朝８時に朝食をとったら、夕食は20時までに食べるということです。ダイエットしたい人はとくに低脂質、低カロリーを心がけましょう。</p>
+
+									<div class="circleTitle">●間食するなら、食物繊維の多いおやつを</div>
+									<p>夕食が遅くなりそうなら、間食としておやつをとるようにしましょう。食物繊維が豊富で糖質控えめなおやつには、夕食時の高血糖を防ぐ効果が期待できます。<br>夕食後どうしてもおなかが空いてしまう人は、食物繊維が豊富なココアなどもよいでしょう。夜の時間帯はカルシウムの吸収効率が日中より高いので、骨密度が心配な高齢者の方にもおすすめです。</p>
+								</div>
+								<div class="imgCol">
+									<figure>
+										<img src="../../assets/img/column1/column_img4.jpg" alt="" width="960" height="919">
+										<figcaption class="center">朝の光を浴びて、体内時計のズレをリセット</figcaption>
+									</figure>
+									<figure>
+										<img src="../../assets/img/column1/column_img5.jpg" alt="" width="960" height="809">
+										<figcaption class="center">食物繊維が豊富で、糖質控えめなおやつがおすすめ</figcaption>
+									</figure>
+									<div class="creditText">制作協力　ＮＨＫエデュケーショナル</div>
+								</div>
+							</div>
+
+							<div class="btnWrap">
+								<a href="https://www.kidzania.jp/membersite/reserve/fee?shop_cd=1" target="_blank" class="btnLink">医師と考える食育一覧へ戻る</a>
+							</div>
+						</div>
+					</div>
+				</main>
+			</div>
+			<!-- /ctArea -->
+
+			<!-- footer -->
+			<?php require_once("../../php/layouts/contents_footer.php"); ?>
+			<!-- /footer -->
+
+		</div>
+		<!-- / innerWrapper -->
+
+	</div>
+	<!-- / wrapper -->
+
+	<?php require_once('../../php/layouts/page_footer.php'); ?>
